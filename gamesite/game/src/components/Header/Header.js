bf3da489158240d3cc26cb57efe5c416944c62b6
mouradbalayev <114390928+muradbalayev@users.noreply.github.com>
2023-05-20 16:01:29 +0400

    Add files via upload

---
+++ gamesite/game/src/components/Header/Header.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import logo from "../../images/logo.png"
+import headerlogo from "../../images/profile-header.jpg"
+function Header() {
+  return (
+   <>
+   <header className="header-area header-sticky">
+    <div className="container">
+        <div className="row">
+            <div className="col-12">
+                <nav className="main-nav">
+                    <a href="!#" className="logo">
+                        <img src={logo} alt=""></img>
+                    </a>
+                    <div className="search-input">
+                      <form id="search" action="#">
+                        <input type="text" placeholder="Type Something" id='searchText' name="searchKeyword" />
+                        <i className="fa fa-search"></i>
+                      </form>
+                    </div>
+                    <ul className="nav">
+                        <li><a href="!#" className="active">Home</a></li>
+                        <li><a href="!#">Browse</a></li>
+                        <li><a href="!#">Details</a></li>
+                        <li><a href="!#">Streams</a></li>
+                        <li><a href="!#">Profile <img src={headerlogo} alt=""></img></a></li>
+                    </ul>   
+                    <a href='!#' className='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                </nav>
+            </div>
+        </div>
+    </div>
+  </header>
+   </>
+  )
+}
+
+export default Header
